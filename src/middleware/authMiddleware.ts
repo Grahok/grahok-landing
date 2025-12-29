b031dfeeb8e06286690a5766258cead464a85a57
Shakil Ahmmed <shakilahmmedwakil.dev@gmail.com>
2025-12-29 16:22:14 +0600

    Simple Better Auth

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,27 @@
+import { checkIsAuthenticatedServer } from "@/features/auth/actions/server";
+import { redirect } from "@tanstack/react-router";
+import { createMiddleware } from "@tanstack/react-start";
+
+export const protectedMiddleware = createMiddleware().server(
+  async ({ next }) => {
+    const isAuthenticated = checkIsAuthenticatedServer();
+
+    if (!isAuthenticated) {
+      throw redirect({ to: "/sign-in" });
+    }
+
+    return await next();
+  }
+);
+
+export const avoidRedundantAuthMiddleware = createMiddleware().server(
+  async ({ next }) => {
+    const isAuthenticated = await checkIsAuthenticatedServer();
+
+    if (isAuthenticated) {
+      throw redirect({ to: "/dashboard" });
+    }
+
+    return await next();
+  }
+);
