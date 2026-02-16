053af13e042a20e8b2c48ff9d5ccae0a222430fc
Shakil Ahmmed <shakilahmmedwakil.dev@gmail.com>
2026-02-16 12:36:34 +0600

    Add PDF export functionality to order details view

--- src/routes/dashboard/orders/view/$orderId.tsx
+++ src/routes/dashboard/orders/view/$orderId.tsx
@@ -1,7 +1,9 @@
 import { Spinner } from "@/components/ui/spinner";
+import { Button } from "@/components/ui/button";
 import OrderDetails from "@/features/orders/components/OrderDetails";
 import { generateMetadata } from "@/lib/tanstack-meta/generator";
 import { createFileRoute } from "@tanstack/react-router";
+import { usePDF } from 'react-to-pdf';
 import { Suspense } from "react";
 
 export const Route = createFileRoute("/dashboard/orders/view/$orderId")({
@@ -17,11 +19,15 @@ export const Route = createFileRoute("/dashboard/orders/view/$orderId")({
 
 function RouteComponent() {
   const { orderId }: { orderId: number } = Route.useParams();
+  const { toPDF, targetRef } = usePDF({filename: 'page.pdf'});
   return (
     <section className="space-y-6">
+      <div className="flex items-center gap-3">
       <h1 className="text-3xl font-bold">Order Details</h1>
+      <Button onClick={() => toPDF()}></Button>
+      </div>
       <Suspense fallback={<Spinner />}>
-        <OrderDetails orderId={orderId} />
+        <OrderDetails pdfRef={targetRef} orderId={orderId} />
       </Suspense>
     </section>
   );
